fb02cd9d4b5951982a3ac6809d6591aa681cb75b
Markus Törnqvist <mjt@fadconsulting.com>
2011-10-15 17:33:52 +0300

    Encoding process

--- ripwhine/processes/__init__.py
+++ ripwhine/processes/__init__.py
@@ -1,2 +1,3 @@
 from ripwhine.processes.rip import start_rip_process
+from ripwhine.processes.encode import start_encode_process
 
---
+++ ripwhine/processes/encode.py
@@ -0,0 +1,62 @@
+# vim: tabstop=4 expandtab autoindent shiftwidth=4 fileencoding=utf-8
+
+import logging
+
+import multiprocessing
+
+logger = multiprocessing.get_logger()
+logger.setLevel(logging.INFO)
+if not logger.handlers:
+    logger.addHandler(logging.StreamHandler())
+
+import subprocess
+
+## FIXME: replace with real encoding cmd ;)
+ENCODE_CMD = 'sleep 3'
+
+class Encode(object):
+    """Process persisting to do encodes on command
+    """
+
+    def __init__(self, interface):
+        """Set interface in self so it can be used later
+        """
+
+        self.interface = interface
+
+        self.actions = (
+            ('START_ENCODE', self.start_encode),
+        )
+
+    def __call__(self):
+        """Make this look like a function
+        """
+
+        logger.info('Encoder process started')
+        while True:
+            command = self.interface.queue_to_encode_interface.recv()
+            logger.info('Encoder received: %s' % command)
+
+            if dict(self.actions).has_key(command):
+                dict(self.actions)[command]()
+
+    def start_encode(self):
+        """Drrn drrn
+        """
+
+        ## Because we wait on subprocess.call(), no need to verify states
+        subprocess.call(ENCODE_CMD.split())
+        logger.info('Finished encode')
+
+        self.interface.queue_to_encode_interface.send('FINISHED_ENCODE')
+
+def start_encode_process(interface):
+    encode = Encode(interface)
+
+    p = multiprocessing.Process(target=encode, name='Encode')
+    p.start()
+
+    return p
+
+# EOF
+
