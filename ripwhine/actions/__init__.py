87fe6cf17cfa7e52bdacfd73fb8428a4f23682b5
Markus Törnqvist <mjt@fadconsulting.com>
2011-10-15 19:32:20 +0300

    Identify action

--- ripwhine/actions/__init__.py
+++ ripwhine/actions/__init__.py
@@ -1,3 +1,4 @@
 from ripwhine.actions.sleep import do_sleep, sleep_process
 from ripwhine.actions.rip import start_rip
+from ripwhine.actions.identify import identify
 
---
+++ ripwhine/actions/identify.py
@@ -0,0 +1,19 @@
+# vim: tabstop=4 expandtab autoindent shiftwidth=4 fileencoding=utf-8
+
+import logging
+
+import multiprocessing
+
+logger = multiprocessing.get_logger()
+logger.setLevel(logging.INFO)
+if not logger.handlers:
+    logger.addHandler(logging.StreamHandler())
+
+def identify(interface):
+    """Identify current disc
+    """
+
+    interface.queue_to_identify.send('IDENTIFY')
+
+# EOF
+
