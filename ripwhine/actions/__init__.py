41b650b3d7087913d9c5f5ff045e062db9b79b8d
Markus Törnqvist <mjt@fadconsulting.com>
2012-02-26 16:25:34 +0200

    Import eject

--- ripwhine/actions/__init__.py
+++ ripwhine/actions/__init__.py
@@ -1,6 +1,7 @@
 from ripwhine.actions.sleep import do_sleep, sleep_process
 from ripwhine.actions.rip import start_rip
 from ripwhine.actions.identify import identify
+from ripwhine.actions.eject import eject
 
 import os
 
